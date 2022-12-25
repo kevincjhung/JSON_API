c650e4407e5e35b9ec65fc2cfd5110b67b90eae0
Kevin Hung <kevincjhung@gmail.com>
2022-12-25 09:12:20 -0800

    init callback branck

--- mysqlDatabase.js
+++ mysqlDatabase.js
@@ -1,88 +1,71 @@
-const mysql = require("mysql2")
+// all code the creates, updates, gets, and deletes tasks should go in this file. 
+// This is a fake database, so just manipulate the array.
+class Database {
+  tasks = [
+    {
+      id: 1,
+      title: "Make vanilla pudding",
+      timestamp: Date.now(),
+      completed: false
+    },
+    {
+      id: 2,
+      title: "Put in mayo jar",
+      timestamp: Date.now(),
+      completed: false
+    },
+    {
+      id: 3,
+      title: "Eat in public",
+      timestamp: Date.now(),
+      completed: false
+    }
+  ]
 
-const pool = mysql
-	.createPool({
-		host: "127.0.0.1",
-		user: "tasklist_app_user",
-		password: "root",
-		database: "tasklist_app",
-	})
+  currentId = 4
 
+  getTasks() {
+    return this.tasks
+  }
 
-function getTasks(cb) {
-	pool.query(`SELECT * FROM tasks`, (error, tasks) => {
-		if(error){
-			callback(error, null)
-			return
-		}
+  /**
+   * @param {number} id 
+   * @returns task with the given id
+   */
+  getTask(id) {
+    return this.tasks.find(task => task.id === id)
+  }
 
-		cb(null, tasks)
-	});
+  addTask(title) {
+    const task = {
+      id: this.currentId,
+      title,
+      timestamp: Date.now(),
+      completed: false
+    }
+    this.currentId++;
+    return task
+  }
+
+  /***
+   * @param {number} id 
+   * @param {string} title
+   * @param {completed} boolean 
+   */
+  updateTask(id, title, completed) {
+    const task = this.tasks.find(task => task.id === id);
+    task.id = id;
+    task.title = title;
+    task.completed = completed;
+
+    return task
+  }
+
+  deleteTask(id) {
+    this.tasks = this.tasks.filter(task => task.id !== id)
+  }
 }
-exports.getTasks = getTasks
+module.exports = Database
 
 
-function getTask(id, cb) {
-	let query = `
-		SELECT * 
-		FROM tasks
-		WHERE id = ?`
 
-	pool.query(query, [id], (error, tasks) => {
-		if (error) {
-			cb(error)
-			return
-		}
-
-		cb(null, tasks[0]);
-	})
-}
-exports.getTask = getTask
-
-
-// after you get the song, pass it back to server.js
-function addTask(title, cb) {
-	let query = `
-		INSERT INTO tasks (title)
-		VALUES (?)`;
-
-	pool.query(query, [title], (error, result) => {
-		if (error) {
-			cb(error)
-			return
-		}
-		const id = result.insertId;
-
-		getTask(id, cb)
-	})
-}
-exports.addTask = addTask
-
-
-function updateTask(id, title, completed, cb) {
-	let query = `
-		UPDATE tasks
-		SET 
-		'title' = ?
-		'completed' = ?
-		WHERE id = ?`;
-
-	pool.query(query, [title], [completed], [id], (error, tasks) => {
-		if (error) {
-			cb(error);
-			return;
-		}
-		cb(null, tasks[0]); // return the task that you updated
-	})
-}
-exports.updateTask = updateTask
-
-
-function deleteTask(id, cb) {
-	let query = `
-		DELETE FROM tasks 
-		WHERE id = '[id]';`
-
-	pool.query(query, [id], cb)
-}
-exports.deleteTask = deleteTask
--- server.js
+++ server.js
@@ -1,17 +1,149 @@
-// *** LIBRARIES *** 
-import * as database from './mysqlDatabaseAsyncAwait.js'
-import makeApp from './app.js'
+// LIBRARIES
+const express = require("express")
+const bodyParser = require("body-parser")
 
+// DATABASE
+const mysqlDatabase = require("./mysqlDatabase")
+const Database = require('./mysqlDatabase');
+const database = new Database();
 
-const app = makeApp(database)
+// EXPRESS
+const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(express.json())
 
+// ROUTES
+// get all the tasks
+app.get("/tasks", (req, res) => {
 
-// PORT and listen
-const port = process.env.PORT || 8080
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  let tasks = database.getTasks((error, tasks) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: "cannot get the task" })
+      return
+    }
+  })
+  res.status(201).send(tasks);
+})
+
+// get single task with id 
+app.get("/tasks/:id", (req, res) => {
+  const id = +req.params.id;
+
+  let task = database.getTask(id, (error, task) => {
+    if (error) {
+      console.error(error);
+      res.status(404).send({ 'error': 'there is an error' })
+      return;
+    }
+
+    if (!task) {
+      res.status(404).send({ error: `there is no task with id of ${id}` })
+    }
+
+  })
+  res.status(200).send(task)
 })
 
 
+// create a new task
+app.post("/tasks", (req, res) => {
 
-// const { reject } = require("bcrypt/promises")
+  const title = req.body.title;
+
+  database.addTask(title, (error, task) => {
+    // if no title, send back 400 status code
+    if (!title.name) {
+      console.error(error);
+      res.status(400).send({ error: "no title" });
+      return;
+    }
+
+    res.status(201).send(task);
+  })
+})
+
+
+// update or replace a task respond with the task object that was updated
+app.put("/tasks/:id", (req, res) => {
+  const id = +req.params.id;
+  const title = req.body.title
+  const completed = req.body.completed;
+  // console.log(completed)
+
+  // if client !give 'title' or 'completed', error
+  if (!title) {
+    res.status(400).send({ "message": "does not contain title" });
+    return;
+  }
+
+  if (!completed) {
+    res.status(400).send({ "message": "does not contain completed" });
+    return;
+  }
+
+  // if receives id of task that !exist, send back a 404 status code with error message.
+  database.getTask(title, (error, task) => {
+    if (error || !task) {
+      console.error(error);
+      res.status(404).send({ message: `there are no tasks with id ${title}` })
+      return
+    }
+  })
+
+  // update the task
+  let updatedTask = database.updateTask(id, req.body, completed, (error, task) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: "cannot get the task" })
+      return
+    }
+  })
+  res.status(200).send(updatedTask);
+})
+
+
+// delete a single task, When successful, this should respond with nothing 
+app.delete("/tasks/:id", (req, res) => {
+  const id = +req.params.id;
+  // const title = req.body.title;
+
+  // if not given an id
+  if (!id) {
+    res.status(400).send({ message: "Give a valid id" })
+    return
+  }
+
+  // if receives id of task that !exist, send back a 404 status code with error message.
+  let task = database.getTask(id, (error, task) => {
+    if (error) {
+      console.error(error);
+      res.status(400).end()
+      return
+    }
+  })
+  
+  if(!task) {
+    console.error(error);
+      res.status(404).send({ message: `there are no tasks with id ${id}` })
+      return
+  }
+  
+  // delete the task
+  let deletedTask = database.deleteTask(id, (error, task) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send({ error: "cannot get the task" })
+      return
+    }
+  })
+
+  res.status(200).end();
+})
+
+
+// PORT and listen
+const port = 8080
+app.listen(port, () => {
+  console.log(`listening on port ${port}`)
+})
